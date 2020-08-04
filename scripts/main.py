68dc0b8d134d1c3da5f171148a812081f926a7e0
Yaroslav Oliinyk <y.oliyn7@gmail.com>
2020-08-04 22:51:51 +0300

    Switched to dev branch. Just making a note to confirm it.

--- scripts/main.py
+++ scripts/main.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-
+# dev branch for coding
 driver = webdriver.Firefox()
 driver.get("http://www.python.org")
 assert "Python" in driver.title
